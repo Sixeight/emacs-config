8c01d3874dcb86838bbc1c24d04639b25a8ad69f
Tomohiro Nishimura <tomohiro68@gmail.com>
2016-01-13 01:17:46 +0900

    慣れないのでとまどった

--- inits/33_smartchr.el
+++ inits/33_smartchr.el
@@ -5,16 +5,16 @@
 (add-hook 'cperl-mode-hook
           '(lambda ()
              (progn
-               (define-key cperl-mode-map (kbd "=") (smartchr '(" = " " => " "=" " == " "==")))
-               (define-key cperl-mode-map (kbd "F") (smartchr '("F" "$" "$_" "$_->" "@$")))
-               (define-key cperl-mode-map (kbd "M") (smartchr '("M" "my $`!!' = ")))
+               ;; (define-key cperl-mode-map (kbd "=") (smartchr '(" = " " => " "=" " == " "==")))
+               ;; (define-key cperl-mode-map (kbd "F") (smartchr '("F" "$" "$_" "$_->" "@$")))
+               ;; (define-key cperl-mode-map (kbd "M") (smartchr '("M" "my $`!!' = ")))
                (define-key cperl-mode-map (kbd "D") (smartchr '("D" "use Data::Dumper; warn Dumper(`!!');")))
-               (define-key cperl-mode-map (kbd "S") (smartchr '("S" "my ($self) = @_;" "my ($self, $`!!') = @_;")))
-               (define-key cperl-mode-map (kbd "C") (smartchr '("C" "my ($class) = @_;" "my ($class, $`!!') = @_;")))
-               (define-key cperl-mode-map (kbd ".") (smartchr '("->" "." "..")))
-               (define-key cperl-mode-map (kbd "|") (smartchr '(" || " "|")))
-               (define-key cperl-mode-map (kbd "&") (smartchr '(" && " "&")))
-               (define-key cperl-mode-map (kbd "{") (smartchr '("{" "sub { `!!' ")))
+               ;; (define-key cperl-mode-map (kbd "S") (smartchr '("S" "my ($self) = @_;" "my ($self, $`!!') = @_;")))
+               ;; (define-key cperl-mode-map (kbd "C") (smartchr '("C" "my ($class) = @_;" "my ($class, $`!!') = @_;")))
+               (define-key cperl-mode-map (kbd ".") (smartchr '("." "->" "..")))
+               ;; (define-key cperl-mode-map (kbd "|") (smartchr '(" || " "|")))
+               ;; (define-key cperl-mode-map (kbd "&") (smartchr '(" && " "&")))
+               ;; (define-key cperl-mode-map (kbd "{") (smartchr '("{" "sub { `!!' ")))
                )))
 
 (add-hook 'ruby-mode-hook
