146a597d5d405027d84defbc9493f01ed62063e6
Tomohiro Nishimura <tomohiro68@gmail.com>
2016-01-17 15:07:27 +0900

    magitのキーバインド追加してけど僕には使えそうにない

--- inits/36_magit.el
+++ inits/36_magit.el
@@ -1,6 +1,7 @@
 
 ;; magit (https://github.com/magit/magit)
-(el-get-bundle magit)
+(el-get-bundle magit
+  (global-set-key (kbd "C-x g") 'magit-status))
 
 ;; git-modes (https://github.com/magit/git-modes)
 (el-get-bundle! magit/git-modes)
